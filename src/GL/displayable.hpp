bcd38a99de85e485ca3119ba7537930237ac18ce
NiOSh77 <benjaminjedrocha@gmail.com>
2021-04-21 16:03:03 +0200

    task-0 C DONE

--- src/GL/displayable.hpp
+++ src/GL/displayable.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <algorithm>
 
 namespace GL {
 
@@ -13,12 +14,21 @@ protected:
     float z = 0;
 
 public:
-    Displayable(const float z_) : z { z_ } {}
-    virtual ~Displayable() {}
+    Displayable(const float z_) : z { z_ } 
+    {
+        display_queue.emplace_back(this);
+    }
+
+    virtual ~Displayable() 
+    {
+        display_queue.erase(std::find(display_queue.begin(), display_queue.end(), this));
+    }
 
     virtual void display() const = 0;
 
     float get_z() const { return z; }
+
+    static inline std::vector<const Displayable*> display_queue;
 };
 
 struct disp_z_cmp
@@ -31,6 +41,6 @@ struct disp_z_cmp
     }
 };
 
-inline std::vector<const Displayable*> display_queue;
+
 
 } // namespace GL
--- src/GL/opengl_interface.cpp
+++ src/GL/opengl_interface.cpp
@@ -57,13 +57,13 @@ void reshape_window(int w, int h)
 void display(void)
 {
     // sort the displayables by their z-coordinate
-    std::sort(display_queue.begin(), display_queue.end(), disp_z_cmp {});
+    std::sort(Displayable::display_queue.begin(), Displayable::display_queue.end(), disp_z_cmp {});
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(-zoom, zoom, -zoom, zoom, 0.0f, 1.0f); // left, right, bottom, top, near, far
     glClear(GL_COLOR_BUFFER_BIT);
     glEnable(GL_TEXTURE_2D);
-    for (const auto& item : display_queue)
+    for (const auto& item : Displayable::display_queue)
     {
         item->display();
     }
@@ -73,9 +73,21 @@ void display(void)
 
 void timer(const int step)
 {
-    for (auto& item : move_queue)
+    if(!is_paused)
     {
-        item->move();
+        for (auto it = move_queue.begin(); it != move_queue.end();)
+        {
+            auto* current = *it;
+            if(current->update())
+            {
+                it++;
+            }
+            else
+            {
+                it = move_queue.erase(it);
+                delete current;
+            }
+        }
     }
     glutPostRedisplay();
     glutTimerFunc(1000u / ticks_per_sec, timer, step + 1);
